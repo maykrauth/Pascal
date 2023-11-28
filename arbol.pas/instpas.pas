0604d021199a4f17ec65f30c37795c9efda24417
Mayra Krauth <maykrauth96@gmail.com>
2023-11-28 19:21:03 -0300

    login hecho

--- arbol.pas/instpas.pas
+++ arbol.pas/instpas.pas
@@ -1,7 +1,7 @@
 program trabajoInstaPas;
 
 Uses 
-    sysutils, DateUtils;
+    sysutils, DateUtils, crt;
 
 
 const MAX= 15;
@@ -79,27 +79,49 @@ begin
     writeln('Nivel 2');
 end;
 
-function verificaUsuarioLogin( Usuario:string; Pass:string; Arbol:arbUsuarios ) : arbUsuarios;
-var auxNodoUsuario: arbUsuarios;
+function BusquedaUser(Arbol:arbUsuarios; NomUser:string):arbUsuarios;
 begin
-    auxNodoUsuario := nil;
-    verificaUsuarioLogin := auxNodoUsuario;
+    if Arbol= nil then
+        BusquedaUser:=nil
+    else
+        if Arbol^.Nombre=NomUser then
+            BusquedaUser:= Arbol
+        else
+            if Arbol^.Nombre<NomUser then
+                BusquedaUser:=BusquedaUser(Arbol^.der,NomUser)
+            else
+                BusquedaUser:= BusquedaUser(Arbol^.izq,NomUser)
+end;
+
+function verificaUsuarioLogin(Usuario:arbUsuarios; Pass:string) : boolean;
+begin
+    if Usuario^.password = Pass then
+        verificaUsuarioLogin := true
+    else
+        verificaUsuarioLogin := false;
 end;
 
 procedure Login(var Arbol:arbUsuarios);
-var Usuario:string[15]; Pass:string[8];
+var Usuario:string[15]; Pass:string[8]; NodoUsuario: arbUsuarios;
 begin
-    writeLn('Ingresar usuario');
+    writeLn('Ingresar nombre usuario');
     readln(Usuario);
     writeLn('Ingresar contraseña');
     readln(Pass);
-    if verificaUsuarioLogin(Usuario, Pass, Arbol) <> nil then
-        Nivel_2()
+    NodoUsuario := BusquedaUser(Arbol, Usuario);
+    if NodoUsuario <> nil then
+        begin
+            if verificaUsuarioLogin(NodoUsuario, Pass) then
+                Nivel_2()
+            else
+                writeLn('pasword incorrecta');
+        end
     else
-        writeLn('Datos de usuario o contraseña invalidos...')
+        writeLn('Usuario invalido')
 end;
+
 procedure Nivel_1 (var ArchSegui:archSeg;var ArchHisto:archHist;var ArchUsuario:archUsu;var Arbol:arbUsuarios;var Lista:arbUsuarios);
-var //Opcion:Topciones;
+var 
     Opcion: integer;
     Salir:Boolean;
 begin
@@ -108,17 +130,17 @@ begin
         writeln('Bienvenido');
         writeln(('1)Login'),'', ('2)Crear NuevoUsuario  '));
         writeln(('3)Indice total de cantidad de Usuarios '),('4) cantidad promedio de Usuarios   '));
-        //writeln(('5) Mostrar Historias') ('6)Salir y guardar datos'));
+        writeln(('5) Mostrar Historias'),('6)Salir y guardar datos'));
         readln(Opcion);
-        //clrscr;
-        {case Opcion of 
-            1:Login(Lista, Arbol, Salir);
-            2:CrearUsuario(Arbol,);
-            3:CantUsuario(Arbol);
-            4:PromedioSeguidores(Arbol)
-            5:UsuarioHistorias(ArchHisto)
+        clrscr;
+        case Opcion of 
+            1:Login(Arbol);
+            //2:CrearUsuario(Arbol,);
+            //3:CantUsuario(Arbol);
+            //4:PromedioSeguidores(Arbol)
+            //5:UsuarioHistorias(ArchHisto)
             6:Salir:=False;
-        end;}
+        end;
     end;
     //Salir(ArchSegui,ArchHisto,ArchUsuario,Arbol,Lista);
     writeln('Programa Finalizado');
@@ -128,4 +150,5 @@ end;
 
 begin
     writeLn('programa principal');
+    Nivel_1(ArchSegui, ArchHisto, ArchUsuario, Arbol, Lista);
 end.
