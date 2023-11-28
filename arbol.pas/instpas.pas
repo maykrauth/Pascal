86291fc67218f75ca4d9d7d4376cbb62beb080d7
Mayra Krauth <maykrauth96@gmail.com>
2023-11-28 18:25:25 -0300

    trab

--- arbol.pas/instpas.pas
+++ arbol.pas/instpas.pas
@@ -1,10 +1,15 @@
 program trabajoInstaPas
 
-Uses sysutils, DateUtils;
+Uses 
+    sysutils, DateUtils;
 
 
 
 const MAX: 15;
+    UbiArchivoUsuario:'/home/Mayra/Documentos/PAscal/ArchivoUsua.dat';
+    UbiArchivoHistorias:'/home/Mayra/Documentos/PAscal/ArchivoHisto.dat';
+    UbiArchivoSeguidos:'/home/Mayra/Documentos/PAscal/archivoSeguidos.dat'; 
+
 
 type 
     arbUsuarios=^nodoArbol;
@@ -50,9 +55,46 @@ type
     archSeguidos=record
         nombredeUsuario:Usuario
     end;
-    archUsu= file of archUsurarios;
-    archSeg= file of archSeguidos;
-    archHist= file of archHistorias;
+    archUsu=file of archUsurarios;
+    archSeg=file of archSeguidos;
+    archHist=file of archHistorias;
 
 /////////////////////////////////////////////Definicion de variables///////////////////////////////////////////////
+var 
+    var Arbol:arbUsuarios;
+        Lista:arbUsuarios;
+        ArchSegui:archSeg;
+        ArchHisto:archHist;
+        ArchUsuario:archUsu;
 
+ 
+procedure CrearUsuario(var Arbol:arbUsuarios;var Salir:Boolean);
+begin
+    NuevoUsuario(Arbol);
+    Salir:=True;
+end;
+
+procedure Nivel_1 (var ArchSegui:archSeg;var ArchHisto:archHist;var ArchUsuario:archUsu;var Arbol:arbUsuarios;var Lista:arbUsuarios);
+var Opcion:Topciones;
+    Salir:Boolean;
+begin
+    Salir:=True;
+    while Salir=True do begin
+        writeln('Bienvenido');
+        writeln(('1)Login  '),'                       ', ('2)Crear NuevoUsuario  '));
+        writeln(('3)Indice total de cantidad de Usuarios '),('4) cantidad promedio de Usuarios   '));
+        writeln(('5) Mostrar Historias') ('6)Salir y guardar datos'))
+        readln(Opcion);
+        clrscr;
+        case Opcion of 
+            1:Login(Lista, Arbol, Salir);
+            2:CrearUsuario(Arbol,);
+            3:CantUsuario(Arbol);
+            4:PromedioSeguidores(Arbol)
+            5:UsuarioHistorias(ArchHisto)
+            6:Salir:=False;
+        end;
+    end;
+    Salir(ArchSegui,ArchHisto,ArchUsuario,Arbol,Lista);
+    writeln('Programa Finalizado');
+end;
