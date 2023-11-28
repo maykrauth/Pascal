a990ac7383f589ae58f7a6ceaee5d556609eb499
Mayra Krauth <maykrauth96@gmail.com>
2023-11-28 18:55:21 -0300

    Compila

--- arbol.pas/instpas.pas
+++ arbol.pas/instpas.pas
@@ -1,17 +1,24 @@
-program trabajoInstaPas
+program trabajoInstaPas;
 
 Uses 
     sysutils, DateUtils;
 
 
+const MAX= 15;
+    UbiArchivoUsuario = '/home/Mayra/Documentos/PAscal/ArchivoUsua.dat';
+    UbiArchivoHistorias= '/home/Mayra/Documentos/PAscal/ArchivoHisto.dat';
+    UbiArchivoSeguidos= '/home/Mayra/Documentos/PAscal/archivoSeguidos.dat'; 
 
-const MAX: 15;
-    UbiArchivoUsuario:'/home/Mayra/Documentos/PAscal/ArchivoUsua.dat';
-    UbiArchivoHistorias:'/home/Mayra/Documentos/PAscal/ArchivoHisto.dat';
-    UbiArchivoSeguidos:'/home/Mayra/Documentos/PAscal/archivoSeguidos.dat'; 
 
+type
 
-type 
+    listHistorias=^nodoHistoria;
+    nodoHistoria=record
+        tipoFecha:TDateTime;
+        texto:string[150];
+        ste:listHistorias
+    end;
+    
     arbUsuarios=^nodoArbol;
     nodoArbol=record
         Nombre:string[15];
@@ -22,23 +29,16 @@ type
         izq, der:arbUsuarios
     end;
 
-    fecha=record
+    {fecha=record
         dia:[1..30];
         mes:[1..12];
         anio:[2023];
         hora:string[15]
-    end;
+    end;}
 
     Usuario=record
         seguidor:string;
         seguido:string
-    end;
-
-    listHistorias=^nodoHistoria;
-    nodoHistoria=record
-        tipoFecha:fecha;
-        texto:string[150];
-        ste:listHistorias
     end;
 
     archUsurarios=record
@@ -48,7 +48,7 @@ type
     end;
 
     archHistorias=record
-        tipoFecha:fecha;
+        tipoFecha:TDateTime;
         texto:string[150]
     end;
     
@@ -60,41 +60,72 @@ type
     archHist=file of archHistorias;
 
 /////////////////////////////////////////////Definicion de variables///////////////////////////////////////////////
-var 
-    var Arbol:arbUsuarios;
+var Arbol:arbUsuarios;
         Lista:arbUsuarios;
         ArchSegui:archSeg;
         ArchHisto:archHist;
         ArchUsuario:archUsu;
 
- 
+
 procedure CrearUsuario(var Arbol:arbUsuarios;var Salir:Boolean);
 begin
-    NuevoUsuario(Arbol);
+    writeLn('Crear usuario');
+    //NuevoUsuario(Arbol);
     Salir:=True;
 end;
 
+procedure Nivel_2();
+begin
+    writeln('Nivel 2');
+end;
+
+function verificaUsuarioLogin( Usuario:string; Pass:string; Arbol:arbUsuarios ) : arbUsuarios;
+var auxNodoUsuario: arbUsuarios;
+begin
+    auxNodoUsuario := nil;
+    verificaUsuarioLogin := auxNodoUsuario;
+end;
+
+procedure Login(var Arbol:arbUsuarios);
+var Usuario:string[15]; Pass:string[8];
+begin
+    writeLn('Ingresar usuario');
+    readln(Usuario);
+    writeLn('Ingresar contraseña');
+    readln(Pass);
+    if verificaUsuarioLogin(Usuario, Pass, Arbol) <> nil then
+        Nivel_2()
+    else
+        writeLn('Datos de usuario o contraseña invalidos...')
+end;
 procedure Nivel_1 (var ArchSegui:archSeg;var ArchHisto:archHist;var ArchUsuario:archUsu;var Arbol:arbUsuarios;var Lista:arbUsuarios);
-var Opcion:Topciones;
+var //Opcion:Topciones;
+    Opcion: integer;
     Salir:Boolean;
 begin
     Salir:=True;
     while Salir=True do begin
         writeln('Bienvenido');
-        writeln(('1)Login  '),'                       ', ('2)Crear NuevoUsuario  '));
+        writeln(('1)Login'),'', ('2)Crear NuevoUsuario  '));
         writeln(('3)Indice total de cantidad de Usuarios '),('4) cantidad promedio de Usuarios   '));
-        writeln(('5) Mostrar Historias') ('6)Salir y guardar datos'))
+        //writeln(('5) Mostrar Historias') ('6)Salir y guardar datos'));
         readln(Opcion);
-        clrscr;
-        case Opcion of 
+        //clrscr;
+        {case Opcion of 
             1:Login(Lista, Arbol, Salir);
             2:CrearUsuario(Arbol,);
             3:CantUsuario(Arbol);
             4:PromedioSeguidores(Arbol)
             5:UsuarioHistorias(ArchHisto)
             6:Salir:=False;
-        end;
+        end;}
     end;
-    Salir(ArchSegui,ArchHisto,ArchUsuario,Arbol,Lista);
+    //Salir(ArchSegui,ArchHisto,ArchUsuario,Arbol,Lista);
     writeln('Programa Finalizado');
 end;
+
+// PROGRAMA PRINCIPAL //
+
+begin
+    writeLn('programa principal');
+end.
