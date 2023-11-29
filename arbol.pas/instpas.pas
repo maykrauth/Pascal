3c2ed5e754adf5f9a2b19714bb3a4c8eb9f028d9
Mayra Krauth <maykrauth96@gmail.com>
2023-11-28 22:50:47 -0300

    Ultima entrega

--- arbol.pas/instpas.pas
+++ arbol.pas/instpas.pas
@@ -11,45 +11,38 @@ const MAX= 15;
 
 
 type
-
+    Dato=string[8];
     listHistorias=^nodoHistoria;
     nodoHistoria=record
-        tipoFecha:TDateTime;
+        fecha:TDateTime;
         texto:string[150];
-        ste:listHistorias
+        sig:listHistorias
     end;
     
     arbUsuarios=^nodoArbol;
     nodoArbol=record
-        Nombre:string[15];
-        password:string[8];
-        email:string[15];
+        Nombre:Dato;
+        password:Dato;
+        email:string;
         usuarios:arbUsuarios;
         historias:listHistorias;
         izq, der:arbUsuarios
     end;
 
-    {fecha=record
-        dia:[1..30];
-        mes:[1..12];
-        anio:[2023];
-        hora:string[15]
-    end;}
-
     Usuario=record
-        seguidor:string;
+        seguidor:Dato;
         seguido:string
     end;
 
     archUsurarios=record
-        Nombre:string[15];
-        password:string[8];
-        emial:string[15]
+        Nombre:Dato;
+        password:Dato;
+        email:string
     end;
 
     archHistorias=record
         tipoFecha:TDateTime;
-        texto:string[150]
+        texto:string
     end;
     
     archSeguidos=record
@@ -67,59 +60,12 @@ var Arbol:arbUsuarios;
         ArchUsuario:archUsu;
 
 
-///////////////////////////////// METODOS NIVEL 2 /////////////////////////////////////////////
-
-
-procedure mostrarHistorias(seguidores: arbUsuarios; fechaInicial: TDateTime);
-begin
-    writeLn('mostrarHistorias');
-    if (seguidores <> nil) then
-        begin
-            writeLn('test')
-        end;
-end;
+/////////////////////// BUSQUEDA USUARIO //////////////////////////////////////////////////////
 
 {
-    Listar las historias de los últimos xxx días de los usuarios que sigo. 
-    Esta opción primero solicita al usuario la cantidad de días hacia atrás que desea ver, 
-    luego muestra por pantalla todas las historias que escribieron los usuarios que sigo desde esa fecha inicial hasta ahora, 
-    para cada uno se muestra, usuario, fecha-hora, e historia. El orden es por usuario (alfabético) y de último a primero por fecha-hora.
+    verifica que el usuario exista y en caso de hacerlo devuelve el puntero a ese usuario caso contrario nil.
 }
-procedure ListarHistorias(usuario: arbUsuarios);
-var diasAtras: integer; fechaInicial, Tiempo2: TDateTime;
-begin
-    Tiempo2 := Now;
-    writeLn('Ingrese la cantidad de dias hacia atras que desea ver');
-    readLn(diasAtras);
-    fechaInicial := IncDay(Now, -diasAtras);
-    mostrarHistorias(usuario^.usuarios, fechaInicial);
-end;
-
-
-////////////////////////////////// NIVEL 2 ////////////////////////////////////////////////
-
-procedure Nivel_2(var Arbol: arbUsuarios; usuarioLogueado:arbUsuarios);
-var Opcion: integer;
-    Salir: boolean;
-begin
-    Salir:=True;
-    while Salir=True do begin
-        writeln('NIVEL 2');
-        writeln('1) Listar las historias de los últimos xxx días de los usuarios que sigo. Esta opción primero solicita al usuario la cantidad de días hacia atrás que desea ver, luego muestra por pantalla todas las historias que escribieron los usuarios que sigo desde esa fecha inicial hasta ahora, para cada uno se muestra, usuario, fecha-hora, e historia. El orden es por usuario (alfabético) y de último a primero por fecha-hora.');
-        readln(Opcion);
-        clrscr;
-        case Opcion of 
-            1:ListarHistorias(usuarioLogueado);
-            //2:CrearUsuario(Arbol);
-            //3:CantUsuario(Arbol);
-            //4:PromedioSeguidores(Arbol)
-            //5:UsuarioHistorias(ArchHisto)
-            6:Salir:=False;
-        end;
-    end;
-end;
-
-function BusquedaUser(Arbol:arbUsuarios; NomUser:string):arbUsuarios;
+function BusquedaUser(Arbol:arbUsuarios; NomUser:Dato):arbUsuarios;
 begin
     if Arbol= nil then
         BusquedaUser:=nil
@@ -133,7 +79,197 @@ begin
                 BusquedaUser:= BusquedaUser(Arbol^.izq,NomUser)
 end;
 
-function verificaUsuarioLogin(Usuario:arbUsuarios; Pass:string) : boolean;
+
+///////////////////////////////// METODOS NIVEL 2 /////////////////////////////////////////////
+
+procedure eliminarNodoLista(var aBorrar, seguidores: arbUsuarios);
+begin
+    if (seguidores <> nil) then
+        begin
+            if (seguidores^.nombre = aBorrar^.nombre) then
+                begin
+                    seguidores := seguidores^.usuarios;
+                    aBorrar^.usuarios := nil;
+                end
+            else
+                eliminarNodoLista(aBorrar, seguidores^.usuarios);
+        end;
+end;
+
+function usuarioSeguido(seguidores: arbUsuarios; nombreSeguidor: Dato):boolean;
+begin
+    if (seguidores = nil) then
+        begin
+            usuarioSeguido := false;
+        end
+    else
+        if (seguidores^.Nombre = nombreSeguidor) then
+            usuarioSeguido := true
+        else
+            usuarioSeguido:= usuarioSeguido(seguidores^.usuarios, nombreSeguidor);
+        
+end;
+
+{
+ Eliminar un usuario de mi lista de seguir. Verificar que esté en mi lista.   
+}
+
+procedure EliminarSeguidor(var Arbol:arbUsuarios; var usuarioLogueado: arbUsuarios);
+var nombSeguidor:Dato; puntSeguidor: arbUsuarios;
+begin
+    writeLn('Indique el nombre del usuario a que quiere dejar de seguir.');
+    readLn(nombSeguidor);
+    puntSeguidor := BusquedaUser(Arbol, nombSeguidor);
+    if (puntSeguidor <> nil) then
+        begin
+            if (usuarioSeguido(usuarioLogueado^.usuarios, nombSeguidor)) then
+                begin
+                    eliminarNodoLista(puntSeguidor, usuarioLogueado^.usuarios);
+                    writeln('Usuario eliminado de la lista de seguidores. (presione una tecla para continuar)');
+                end;
+            //insertarOrdenadoListaSeguidores(usuarioLogueado^.usuarios, puntNuevoSeguidor);
+            readln();
+        end
+    else
+        writeLn('el usuario no existe.')
+end;
+
+procedure insertarOrdenadoListaSeguidores(var seguidores:arbUsuarios; nuevoSeguidor: arbUsuarios);
+begin
+    if (seguidores = nil) or (seguidores^.nombre > nuevoSeguidor^.nombre) then
+        begin
+            nuevoSeguidor^.usuarios := seguidores; 
+            seguidores := nuevoSeguidor;
+        end
+    else
+        insertarOrdenadoListaSeguidores(seguidores^.usuarios,nuevoSeguidor);
+end;
+
+{
+    Agregar un usuario a mi lista de seguir. Verificar que exista y no esté en mi lista.
+}
+
+procedure AgregarSeguidor(var Arbol:arbUsuarios; usuarioLogueado: arbUsuarios);
+var nombNuevoSeguidor:Dato; puntNuevoSeguidor: arbUsuarios;
+begin
+    writeLn('Indique el nombre del usuario a seguir.');
+    readLn(nombNuevoSeguidor);
+    puntNuevoSeguidor := BusquedaUser(Arbol, nombNuevoSeguidor);
+    if (puntNuevoSeguidor <> nil) then
+        begin
+            insertarOrdenadoListaSeguidores(usuarioLogueado^.usuarios, puntNuevoSeguidor);
+            writeln('Usuario agregado a la lista. (presione una tecla para continuar)');
+            readln();
+        end
+    else
+        writeLn('el usuario no existe.')
+end;
+
+
+{
+    Listar los usuarios que sigo
+}
+
+procedure ListarSeguidores(seguidores:arbUsuarios);
+begin
+    if (seguidores <> nil) then
+        begin
+            writeLn('Seguidor : ', seguidores^.nombre);
+            ListarSeguidores(seguidores^.usuarios);
+        end
+end;
+
+function insertarPrimero(var historias: listHistorias; nuevaHistoria: listHistorias):listHistorias;
+begin
+    nuevaHistoria^.sig := historias;
+    historias := nuevaHistoria;
+end;
+
+{
+    Escribir una historia: se pide el texto y se carga en la lista incorporando la fecha y hora actual. 
+}
+procedure escribirHistoria(var usuarioLogueado: arbUsuarios);
+var texto:string[150]; auxNodoHistoria: listHistorias;
+begin
+    auxNodoHistoria := nil;
+    writeLn('Ingrese el mensaje de la historia');
+    readLn(texto);
+    new(auxNodoHistoria);
+    auxNodoHistoria^.texto := texto;
+    auxNodoHistoria^.fecha := Now;
+    auxNodoHistoria^.sig := nil;
+    insertarPrimero(usuarioLogueado^.historias, auxNodoHistoria);
+end;
+
+procedure mostrarHistorias(historias:listHistorias; fechaInicial: TDateTime);
+begin
+    if (historias <> nil) then
+        begin
+            if (historias^.fecha >= fechaInicial) then
+                begin
+                    writeLn('Historia : ', historias^.texto);
+                end;
+            mostrarHistorias(historias^.sig, fechaInicial);
+        end
+end;
+
+procedure recorrerSeguidores(seguidores: arbUsuarios; fechaInicial: TDateTime);
+begin
+    writeLn('recorrerSeguidores');
+    if (seguidores <> nil) then
+        begin
+            writeln('entra aca ?');
+            mostrarHistorias(seguidores^.historias, fechaInicial);
+            recorrerSeguidores(seguidores^.usuarios, fechaInicial);
+        end
+end;
+
+{
+    Listar las historias de los últimos xxx días de los usuarios que sigo. 
+    Esta opción primero solicita al usuario la cantidad de días hacia atrás que desea ver, 
+    luego muestra por pantalla todas las historias que escribieron los usuarios que sigo desde esa fecha inicial hasta ahora, 
+    para cada uno se muestra, usuario, fecha-hora, e historia. El orden es por usuario (alfabético) y de último a primero por fecha-hora.
+}
+procedure ListarHistorias(usuarioLogueado: arbUsuarios);
+var diasAtras: integer; fechaInicial, Tiempo2: TDateTime;
+begin
+    Tiempo2 := Now;
+    writeLn('Ingrese la cantidad de dias hacia atras que desea ver');
+    readLn(diasAtras);
+    fechaInicial := IncDay(Now, -diasAtras);
+    recorrerSeguidores(usuarioLogueado^.usuarios, fechaInicial);
+end;
+
+
+////////////////////////////////// NIVEL 2 ////////////////////////////////////////////////
+
+procedure Nivel_2(var Arbol: arbUsuarios; usuarioLogueado:arbUsuarios);
+var Opcion: integer;
+    Salir: boolean;
+begin
+    Salir:=True;
+    while Salir=True do begin
+        writeln('NIVEL 2');
+        writeln('1) Listar las historias de los últimos xxx días de los usuarios que sigo.');
+        writeln('2) Escribir una historia');
+        writeln('3) Listar los usuarios que sigo');
+        writeln('4) Agregar un usuario a mi lista de seguir');
+        writeLn('5) Eliminar un usuario de mi lista de seguir');
+        writeLn('6) Logout');
+        readln(Opcion);
+        clrscr;
+        case Opcion of 
+            1:ListarHistorias(usuarioLogueado);
+            2:escribirHistoria(usuarioLogueado);
+            3:ListarSeguidores(usuarioLogueado^.usuarios);
+            4:AgregarSeguidor(Arbol, usuarioLogueado);
+            5:EliminarSeguidor(Arbol, usuarioLogueado);
+            6:Salir:=False;
+        end;
+    end;
+end;
+
+function verificaUsuarioLogin(Usuario:arbUsuarios; Pass:Dato) : boolean;
 begin
     if Usuario^.password = Pass then
         verificaUsuarioLogin := true
@@ -143,7 +279,7 @@ end;
 
 // Verifica que el usuario se encuentre en el arbol y la contraseña sea correcta //
 procedure Login(var Arbol:arbUsuarios);
-var Usuario:string[15]; Pass:string[8]; NodoUsuario: arbUsuarios;
+var Usuario:Dato; Pass:Dato; NodoUsuario: arbUsuarios;
 begin
     writeLn('Ingresar nombre usuario');
     readln(Usuario);
@@ -166,16 +302,16 @@ begin
     if (Arbol <> nil) then
         begin
             if (Arbol^.nombre > nuevoUsuario^.nombre) then
-                InsertarOrdenado(Arbol^.der, nuevoUsuario)
-            else
                 InsertarOrdenado(Arbol^.izq, nuevoUsuario)
+            else
+                InsertarOrdenado(Arbol^.der, nuevoUsuario)
         end
     else
         Arbol:= nuevoUsuario;
 end;
 
 procedure CrearUsuario(var Arbol: arbUsuarios);
-var Usuario, Pass, email: string; nuevoUsuario: arbUsuarios;
+var Usuario, Pass, email: Dato; nuevoUsuario: arbUsuarios;
 begin
     writeLn('Ingresar nombre usuario');
     readLn(Usuario);
@@ -223,10 +359,8 @@ end;
 
 
 procedure CantidadUsuarios(Arbol: arbUsuarios);
-var test : integer;
 begin
-    test := CantUsuarios(Arbol);
-    writeLn('test ', test);
+    writeLn('Cantidad total de Usuarios : ', CantUsuarios(Arbol));
     writeLn('Presione cualquier tecla para continuar ');
     readLn();
     clrscr;
